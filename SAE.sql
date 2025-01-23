4ee3ca6ef6e7698a159ab1cfd837a9fac15a5bff
pchassig <paul.chassignol@edu.univ-fcomte.fr>
2025-01-23 15:17:36 +0100

    modifs sql

--- SAE.sql
+++ SAE.sql
@@ -3,9 +3,10 @@ DROP TABLE IF EXISTS ligne_commande;
 DROP TABLE IF EXISTS lunette;
 DROP TABLE IF EXISTS categorie;
 DROP TABLE IF EXISTS couleur;
+DROP TABLE IF EXISTS commande;
 DROP TABLE IF EXISTS etat;
 DROP TABLE IF EXISTS utilisateur;
-DROP TABLE IF EXISTS commande;
+
 
 
 CREATE TABLE utilisateur(
@@ -13,8 +14,9 @@ CREATE TABLE utilisateur(
    login VARCHAR(50),
    email VARCHAR(50),
    nom VARCHAR(50),
-   password VARCHAR(50),
+   password VARCHAR(255),
    role VARCHAR(50),
+    est_actif VARCHAR(255),
    PRIMARY KEY(id_utilisateur)
 );
 
@@ -81,3 +83,18 @@ CREATE TABLE ligne_panier(
    FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur),
    FOREIGN KEY(id_lunette) REFERENCES lunette(id_lunette)
 );
+
+
+
+INSERT INTO utilisateur(id_utilisateur,login,email,password,role,nom,est_actif) VALUES
+(1,'admin','admin@admin.fr',
+    'sha256$dPL3oH9ug1wjJqva$2b341da75a4257607c841eb0dbbacb76e780f4015f0499bb1a164de2a893fdbf',
+    'ROLE_admin','admin','1'),
+(2,'client','client@client.fr',
+    'sha256$1GAmexw1DkXqlTKK$31d359e9adeea1154f24491edaa55000ee248f290b49b7420ced542c1bf4cf7d',
+    'ROLE_client','client','1'),
+(3,'client2','client2@client2.fr',
+    'sha256$MjhdGuDELhI82lKY$2161be4a68a9f236a27781a7f981a531d11fdc50e4112d912a7754de2dfa0422',
+    'ROLE_client','client2','1');
+
+
